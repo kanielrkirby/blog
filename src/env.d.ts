49aa9b8e8b50b377be3e1ca8aeb27fe44c159519
Kaniel Kirby <pirate7007@runbox.com>
2024-07-18 19:23:35 -0500

    fix: types for `scramble`
    Added `.d.ts` file for types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,30 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="astro/astro-jsx" />
+declare namespace astroHTML.JSX {
+  interface IntrinsicAttributes {
+    scramble?: number | boolean
+  }
+  interface HTMLAttributes {
+    scramble?: number | boolean
+  }
+  interface IntrinsicElements {
+    time: DetailedHTMLProps<TimeHTMLAttributes<HTMLTimeElement>, HTMLTimeElement> & {
+      scramble?: boolean;
+    };
+    time: DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> & {
+      scramble?: boolean;
+    };
+  }
+}
+
+declare namespace React {
+  interface Attributes {
+      scramble?: boolean;
+  }
+}
+
+interface Text {
+  /** Custom attribute specifically for scrambling text with `scramble()` */
+  __trueTextContent: undefined | string;
+}
