b22d4bc7af3121315c9536ef711cef23ae091478
Kaniel Kirby <pirate7007@runbox.com>
2024-07-18 19:22:40 -0500

    refactor: refactor Astro Paper, branding overhaul
    There were lots of things I would never use, didn't like, or just needed to rework in general, and this commit contains most of the smaller ones.
    
    Also includes some branding overhauls.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,21 +11,16 @@ export interface Props {
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, published, modified, desc } = frontmatter;
 
-  const headerProps = {
-    style: { viewTransitionName: slugifyStr(title) },
-    className: "text-lg font-medium decoration-dashed hover:underline",
-  };
-
   return (
     <li className="my-6">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="font-mono font-bold inline-block text-lg text-skin-base-faded hover:text-skin-accent transition-all duration-100 decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
-          <h2 {...headerProps}>{title}</h2>
+          <h2>{title}</h2>
         ) : (
-          <h3 {...headerProps}>{title}</h3>
+          <h3>{title}</h3>
         )}
       </a>
       <Datetime published={published} modified={modified} />
